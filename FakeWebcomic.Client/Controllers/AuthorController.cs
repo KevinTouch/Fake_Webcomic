2180fb163b4fd3d5ff9b60edf5445992f9c3576b
elliot <elliot.reid@revature.net>
2021-02-03 22:28:06 -0800

    fixed null-reference exception from 0 pages, added author sign-in and homepage, added main FakeWebcomic navbar to the normal layout (above the normal layout navbar)

--- FakeWebcomic.Client/Controllers/AuthorController.cs
+++ FakeWebcomic.Client/Controllers/AuthorController.cs
@@ -1,20 +1,44 @@
-using System.Threading.Tasks;
+using FakeWebcomic.Client.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace FakeWebcomic.Client.Controllers
 {
     [Route("[controller]")]
     public class AuthorController : Controller
     {
-        // [HttpGet]
-        // [Authorize]
-        // public async Task<IActionResult> AuthorHome()
-        // {
-        //     //search for author object in api based on User.Identity.Name
-        //     //Alternatively, retrieve all webcomics with Author == User.Identity.Name
-        //     return await Task<IActionResult>();
-        // }
+        private string _webcomicsUri = "https://localhost:5001/api/comicbook";
+        private HttpClientHandler _clientHandler = new HttpClientHandler();
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> AuthorHome()
+        {
+            _clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            using (var _http = new HttpClient(_clientHandler))
+            {
+                var response = await _http.GetAsync(_webcomicsUri);
+                if (response.IsSuccessStatusCode)
+                {
+                    List<ComicBookModel> AuthorsComicBooks = new List<ComicBookModel>{};
+                    var ComicBooks = JsonConvert.DeserializeObject<List<ComicBookModel>>(await response.Content.ReadAsStringAsync());
+                    if (ComicBooks != null)
+                    {
+                        AuthorsComicBooks = ComicBooks.FindAll(c => c.Title == User.Identity.Name);
+                        AuthorsComicBooks.OrderBy(c => c.Title);
+                    }
+                    return View("AuthorHomeView", new AuthorHomeViewModel(User.Identity.Name, AuthorsComicBooks));
+                }
+                return View("Error", new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+        }
 
         // [HttpPost]
         // public async Task<IActionResult> NewAuthor()
